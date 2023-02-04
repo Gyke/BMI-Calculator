6939f7382fe1a6a02668750b2e4c83b223b69df8
Konstantin <don.kamatoz@bk.ru>
2023-02-04 20:21:28 +0500

    Добавил вывод текущего значения слайдера в соответствующий лейбл

--- BMI Calculator/Controllers/ViewController.swift
+++ BMI Calculator/Controllers/ViewController.swift
@@ -10,6 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var weightValueView: UILabel!
+    @IBOutlet weak var heightValueView: UILabel!
+    
+    @IBOutlet weak var heightSliderView: UISlider!
+    @IBOutlet weak var weightSliderView: UISlider!
+    
+    @IBAction func sliderValueChanged(_ sender: UISlider) {
+        if sender == heightSliderView {
+            heightValueView.text = String(format: "%.2f", heightSliderView.value) + " m"
+        } else {
+            weightValueView.text = String(format: "%.0f", weightSliderView.value) + " Kg"
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
